599057ddba35a2842a7644663e8369be71b98732
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-05-07 20:29:37 -0300

    Se agrega el ejericio N°1

---
+++ main.js
@@ -0,0 +1,15 @@
+function esperar(tiempo) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+        resolve(`Esperado ${tiempo} ms`);
+        }, tiempo);
+        });
+    }
+
+esperar(2000)
+    .then((resultado) => {
+        console.log(resultado);
+        })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
